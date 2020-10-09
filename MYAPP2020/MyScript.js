2b8315dfb0e893b7d331439d3f44e8aca2cc824f
Wilber1987 <wilberj1987@gmail.com>
2020-10-09 17:46:13 -0600

    30-10

---
+++ MYAPP2020/MyScript.js
@@ -0,0 +1,13 @@
+
+import {suma, resta, multiplicar} from './modulos/HTML';
+
+function name() {   
+    var a = "hola"; 
+    const v = /*html*/`
+        <div>xdfksdf ${a}</div>                  
+        `;  
+        const v = /*html*/`<div>xdfksdf ${a}</div>`;         
+        miDiv.append(v);    
+}
+
+
---
+++ MYAPP2020/modulos/HTML.js
@@ -0,0 +1,9 @@
+export var suma = function (a, b) {
+    return a + b;
+}
+
+export var resta = (a, b) => a - b;
+
+export var multiplicar = (a, b) => a * b;
+
+//etc
